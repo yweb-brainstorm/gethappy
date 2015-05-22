bf84de0ea178a7cf36a1693210793c20425ff98d
Byron6 <byrshaw@gmail.com>
2015-05-21 19:39:40 -0500

    working activity box

--- Lifecycle app/lcHomePage.js
+++ Lifecycle app/lcHomePage.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
 	var  mn = $(".main-nav");
-    mns = "main-nav-scrolled";
-    hdr = $('header').height();
+  mns = "main-nav-scrolled";
+  hdr = $('header').height();
 
-$(window).scroll(function() {
-  if( $(this).scrollTop() > hdr ) {
-    mn.addClass(mns);
-    mn.removeClass('.main-nav');
-  } else {
-    mn.removeClass(mns);
-  }
-});
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > hdr ) {
+      mn.addClass(mns);
+      mn.removeClass('.main-nav');
+    } else {
+      mn.removeClass(mns);
+    }
+  });
+  $('.activities').on('click', function(){
+    $('ul.activities').prepend('<li class="newActivity">new item</li>');
+  });
 });
