fcf9811b33a6479b9a8cba4b28bea140d51961a4
Byron6 <byrshaw@gmail.com>
2015-05-24 15:21:37 -0500

    finished status bar

--- js/app.js
+++ js/app.js
@@ -16,4 +16,37 @@ $(document).ready(function(){
 //   $('.activities').on('click', function(){
 //     $('ul.activities').prepend('<li class="stat-bar">50%</li>');
 //   });
-// });
+$("#set").click(function() {
+  var value = $("#value").val();
+  console.log(value);
+  var $progressbar = $("#progressbar > div");
+  $progressbar.animate({
+    "width": value + "%"
+  }, 2000, "linear", function() {
+    if (value == 100) {
+      var $color = $progressbar.css("background-color");
+      $progressbar.animate({
+        "background-color": "#33f153"
+      }, 600, function() {
+        $progressbar.animate({
+          "background-color": $color
+        });
+      });
+    }
+  });
+});
+
+$("#reset").click(function() {
+  var $progressbar = $("#progressbar > div");
+  $progressbar.css("width", 0);
+});
+
+$("#button").click(function() {
+  var $progressbar = $("#progressbar > div");
+  $progressbar
+  .css("width", 0)
+  .animate({
+    "width": "100%"
+  }, 6000, "linear");
+});
+});
